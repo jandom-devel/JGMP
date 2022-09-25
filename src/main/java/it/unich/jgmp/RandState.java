eb58cab1da32ad6e968dee6e19da443311bcf525
Gianluca Amato <gianluca.amato.74@gmail.com>
2022-09-25 18:55:36 +0200

    Add copyright notices.

--- src/main/java/it/unich/jgmp/RandState.java
+++ src/main/java/it/unich/jgmp/RandState.java
@@ -1,3 +1,20 @@
+/**
+* Copyright 2022 Gianluca Amato <gianluca.amato@unich.it>
+*
+* This file is part of JGMP. JGMP is free software: you can
+* redistribute it and/or modify it under the terms of the GNU General Public
+* License as published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* JGMP is distributed in the hope that it will be useful, but WITHOUT ANY
+* WARRANTY; without even the implied warranty of a MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+* details.
+*
+* You should have received a copy of the GNU General Public License along with
+* JGMP. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package it.unich.jgmp;
 
 import static it.unich.jgmp.nativelib.LibGMP.__gmp_randclear;
@@ -150,9 +167,9 @@ public class RandState {
     }
 
     /**
-     * Return a uniformly distributed random number of {@code n} bits, i.e. in the range {@code 0}
-     * to <code>(2<sup>n</sup>-1)</code> inclusive. {@code n} must be less than or equal to the
-     * number of bits in a native unsigned long.
+     * Return a uniformly distributed random number of {@code n} bits, i.e. in the
+     * range {@code 0} to <code>(2<sup>n</sup>-1)</code> inclusive. {@code n} must
+     * be less than or equal to the number of bits in a native unsigned long.
      *
      * @apiNote {@code n} should be treated as an unsigned long.
      */
@@ -161,8 +178,8 @@ public class RandState {
     }
 
     /**
-     * Return a uniformly distributed random number in the range {@code 0}
-     * to {@code (n - 1)} inclusive.
+     * Return a uniformly distributed random number in the range {@code 0} to
+     * {@code (n - 1)} inclusive.
      *
      * @apiNote {@code n} should be treated as an unsigned long.
      */
